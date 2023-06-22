59f8df5c44c83ba0461e009882d4919260759067
Luca Rossi <lucarossi147@gmail.com>
2023-06-22 16:49:37 +0200

    added methods for acquiring data

--- src/e384commlib_python.cpp
+++ src/e384commlib_python.cpp
@@ -6,6 +6,14 @@
 namespace py = pybind11;
 using namespace e384CommLib;
 
+static MessageDispatcher * md;
+static int16_t * data;
+
+static int voltageChannelsNum;
+static int currentChannelsNum;
+static int totalChannelsNum;
+static std::vector<std::vector<double>> voltageDataOut;
+static std::vector<std::vector<double>> currentDataOut;
 PYBIND11_MODULE(e384CommLibPython, m) {
     m.doc() = "Typical order of calls\n"
               "detectDevices();\n"
@@ -18,6 +26,65 @@ PYBIND11_MODULE(e384CommLibPython, m) {
         ErrorCodes_t err = MessageDispatcher::detectDevices(deviceIds);
         return std::make_tuple(err, deviceIds);
     }, "Detect plugged in devices");
+
+    m.def("connectDevice",[](std::string deviceName){
+        ErrorCodes_t ret = md->allocateRxDataBuffer(data);
+        if (ret != Success) {
+            return ret;
+        }
+        ret = MessageDispatcher::connectDevice(deviceName, md);
+        if (ret != Success) {
+            return ret;
+        }
+        uint16_t vNum;
+        uint16_t cNum;
+        ret = md->getChannelNumberFeatures(vNum, cNum);
+        if (ret != Success) {
+            return ret;
+        }
+        voltageChannelsNum = static_cast<int>(vNum);
+        currentChannelsNum = static_cast<int>(cNum);
+        totalChannelsNum = currentChannelsNum + voltageChannelsNum;
+        return ret;
+    }, "Connect to one of the plugged in device");
+
+    m.def("getSamplingRate",[](){
+        std::vector <Measurement_t> samplingRates;
+        ErrorCodes_t err = md->getSamplingRatesFeatures(samplingRates);
+        return  std::make_tuple(err, samplingRates);
+    }, "Get all the sampling rate the device can handle");
+
+    m.def("setVoltageHoldTuner",[](std::vector<uint16_t> channelIndexes, std::vector<Measurement_t> voltages, bool applyFlag){
+        return md->setVoltageHoldTuner(channelIndexes, voltages, applyFlag);
+    });
+
+    m.def("getBufferedVoltagesAndCurrents", [](){
+        RxOutput_t rxOutput;
+        ErrorCodes_t err = md->getNextMessage(rxOutput, data);
+        voltageDataOut.resize(rxOutput.dataLen/totalChannelsNum);
+        for (int i=0; i < voltageDataOut.size(); i++) {
+            voltageDataOut[i].resize(voltageChannelsNum);
+        }
+        currentDataOut.resize(rxOutput.dataLen/totalChannelsNum);
+        for (int i=0; i < currentDataOut.size(); i++) {
+            currentDataOut[i].resize(currentChannelsNum);
+        }
+        if (err!= Success){
+            return std::make_tuple(err, voltageDataOut, currentDataOut);
+        }
+        if (rxOutput.msgTypeId != MsgDirectionDeviceToPc+MsgTypeIdAcquisitionData){
+            return std::make_tuple(ErrorNoDataAvailable, voltageDataOut, currentDataOut);
+        }
+        for (unsigned long wordsIdx = 0; wordsIdx < rxOutput.dataLen; wordsIdx += totalChannelsNum) {
+            for (int chIdx = 0; chIdx < voltageChannelsNum; chIdx++) {
+                md->convertVoltageValue(data[wordsIdx+chIdx], voltageDataOut[wordsIdx/totalChannelsNum][chIdx]);
+            }
+            for (int chIdx = 0; chIdx < currentChannelsNum; chIdx++) {
+                md->convertCurrentValue(data[wordsIdx+chIdx+voltageChannelsNum], currentDataOut[wordsIdx/totalChannelsNum][chIdx]);
+            }
+        }
+        return std::make_tuple(err, voltageDataOut, currentDataOut);
+    }, "Get buffered voltage and current values");
 
 //    todo completare gli error codes
     py::enum_<ErrorCodes_t>(m, "ErrorCodes")
@@ -64,4 +131,15 @@ PYBIND11_MODULE(e384CommLibPython, m) {
             .def_readonly("value", &Measurement_t::value)
             .def_readonly("prefix", &Measurement_t::prefix)
             .def_readonly("unit", &Measurement_t::unit);
+
+//    py::class_<RxOutput>(m, "RxOutput")
+//            .def(py::init<const std::string &>())
+//            .def_readonly("msgTypeId", &RxOutput::msgTypeId)
+//            .def_readonly("channelIdx", &RxOutput::channelIdx)
+//            .def_readonly("protocolId", &RxOutput::protocolId)
+//            .def_readonly("protocolItemIdx", &RxOutput::protocolItemIdx)
+//            .def_readonly("protocolRepsIdx", &RxOutput::protocolRepsIdx)
+//            .def_readonly("protocolSweepIdx", &RxOutput::protocolSweepIdx)
+//            .def_readonly("firstSampleOffset", &RxOutput::firstSampleOffset)
+//            .def_readonly("dataLen", &RxOutput::dataLen);
 }
