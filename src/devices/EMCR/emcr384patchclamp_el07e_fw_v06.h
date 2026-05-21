a30ddb083a8a53963109a2fefc80f6e05dc4ee42
fcona <fcona@elements-ic.com>
2026-05-21 17:20:28 +0200

    fix: added missed file

---
+++ src/devices/EMCR/emcr384patchclamp_el07e_fw_v06.h
@@ -0,0 +1,11 @@
+#ifndef EMCR384PATCHCLAMP_EL_7E_FW_V_6_H
+#define EMCR384PATCHCLAMP_EL_7E_FW_V_6_H
+
+#include "emcr384patchclamp_el07e_fw_v05.h"
+
+class Emcr384PatchClamp_EL07e_fw_v06 : public Emcr384PatchClamp_EL07e_fw_v05 {
+public:
+    Emcr384PatchClamp_EL07e_fw_v06(std::string di);
+};
+
+#endif // EMCR384PATCHCLAMP_EL_7E_FW_V_6_H
